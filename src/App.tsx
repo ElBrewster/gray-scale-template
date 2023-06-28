547783382b4f4ebf165a775de64863807dbc1e6b
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-06-28 13:05:29 -0700

    Add Route and Routes to app file and path to layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-
+import { Routes, Route } from "react-router"
+import Layout from "./components/Layout"
 
 export default function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
 
+        </Route>
+      </Routes>
     </div>
   )
 }
