2e8dd2fd92e3c77fbb8b7a3ef4583aa213a572ab
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-06-28 13:38:17 -0700

    Add files and routes for page views to assist nav styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Routes, Route } from "react-router"
 import Layout from "./components/Layout"
+import Profile from "./pages/Profile";
+import Scoreboard from "./pages/Scoreboard";
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index path="/" element={}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/scoreboard" element={<Scoreboard />}></Route>
           {/* pages go here */}
         </Route>
       </Routes>
---
+++ src/pages/Scoreboard.tsx
@@ -0,0 +1,5 @@
+export default function Scoreboard() {
+    return (
+        <h2>Scoreboard Page</h2>
+    );
+}
