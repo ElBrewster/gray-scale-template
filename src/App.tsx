ce5ec8265f7d7a9539afa9edefac5c7917e2d2b6
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-06-28 12:53:56 -0700

    Clean out boilerplate vite code and remove unneccessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export default function App() {
   return (
     <main>
 
@@ -13,4 +8,3 @@ function App() {
   )
 }
 
-export default App
