c65188ec3186c74545dc3d2d2a3b346d89027ded
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-06-28 13:40:55 -0700

    Implement routes for page views of template site

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { Routes, Route } from "react-router"
 import Layout from "./components/Layout"
 import Profile from "./pages/Profile";
 import Scoreboard from "./pages/Scoreboard";
+import Home from "./pages/Home";
+import Form from "./pages/Form";
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index path="/" element={}></Route>
+          <Route index path="/" element={<Home />}></Route>
+          <Route path="/form" element={<Form />}></Route>
           <Route path="/profile" element={<Profile />}></Route>
           <Route path="/scoreboard" element={<Scoreboard />}></Route>
           {/* pages go here */}
