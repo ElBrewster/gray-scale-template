d0abafd604fd77e411bd280fbb49a25ca6ac9952
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-06-29 13:49:09 -0700

    Add aside component to layout and move nav component into aside

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router";
 import Footer from "./Footer";
 import Header from "./Header";
-import Nav from "./Nav";
+import Aside from "./Aside";
 
 export default function Layout() {
     return(
         <div>
+            <a href="main-content">Skip to the main content.</a>
             <Header />
-            <Nav />
+            <Aside />
             <main id="main-content">
                 <Outlet /> 
             </main>
