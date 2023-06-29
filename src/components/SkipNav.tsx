b13cf6b1327674c3276c410a9c111a62961027cf
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-06-29 14:31:25 -0700

    Add hidden functionality to skipnav component with css

--- src/components/SkipNav.tsx
+++ src/components/SkipNav.tsx
@@ -1,5 +1,8 @@
 export default function SkipNav() {
+    function handleFocus(){
+        console.log("focus skipnav component");
+    }
     return (
-        <a href="main-content" hidden>Skip to the main content.</a>
+        <a href="main-content" className="skip-link" id="delay" onFocus={handleFocus}>Skip to main content.</a>
     );
 }
