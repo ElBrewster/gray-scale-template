14127d3869a59244889cdaa0b81c6824baa79b50
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-07-01 14:15:49 -0700

    Format home page so as to add active and hover states to link element

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,11 @@ import Button from "../components/Button";
 
 export default function Home() {
     return(
-        <div>
+        <div className="home-page-container">
             <h2>Home Page</h2>
-            <Link to="/form" className="link-button">Get Devil Form</Link>
+            <div className="button-box">
+                <Link to="/form" className="link-button">Get Devil Form</Link>
+            </div>
             <div className="button-box">
                 <Button />
                 <button className="button-default">"default"</button>
