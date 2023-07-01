42e30a94367e5f0a423d6dd45c4fde4684055c96
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-07-01 13:51:18 -0700

    Add button component as well as several generic button elements to demo button states

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom";
+import Button from "../components/Button";
+
 
 export default function Home() {
     return(
         <div>
             <h2>Home Page</h2>
             <Link to="/form" className="link-button">Get Devil Form</Link>
+            <div className="button-box">
+                <Button />
+                <button className="button-default">"default"</button>
+                <button className="button-hover">"hover"</button>
+                <button className="button-active">"active"</button>
+            </div>
         </div>
     );
 }
