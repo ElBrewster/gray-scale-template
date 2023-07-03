9b67ff12c34d73d3829666b4516825565ba78488
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-07-03 08:03:01 -0700

    Add select options and select container and label to form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,17 +3,25 @@ export default function Form() {
         <form className="my-form-container">
             <div className="input-container">
                 <label htmlFor="my-input" className="input-label"></label>
-                <input type="text" name="my-input"/>
+                <input type="text" name="my-input" placeholder="my input"/>
             </div>
             <div className="input-container">
                 <label htmlFor="my-input" className="input-label"></label>
-                <input type="text" name="my-input"/>
+                <input type="text" name="my-input" placeholder="my input"/>
             </div>
             <div className="input-container">
                 <label htmlFor="my-input" className="input-label"></label>
-                <input type="text" name="my-input"/>
+                <input type="text" name="my-input" placeholder="my input"/>
             </div>
-            <div className="input-container"></div>
+            <div className="select-container">
+                <label htmlFor="my-select" className="select-label"></label>
+                <select name="my-slect" id="mySelect">
+                    <option value="one">one</option>
+                    <option value="two">two</option>
+                    <option value="three">three</option>
+                    <option value="four">four</option>
+                </select>
+            </div>
             <div className="input-container"></div>
             <div className="input-container"></div>
             <label htmlFor="my-input">YO
