6cfabeef7158c895212b9d2dd493d687d191950f
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-07-03 07:53:15 -0700

    Add input containers labels and inputs to form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,21 @@
 export default function Form() {
     return (
         <form className="my-form-container">
+            <div className="input-container">
+                <label htmlFor="my-input" className="input-label"></label>
+                <input type="text" name="my-input"/>
+            </div>
+            <div className="input-container">
+                <label htmlFor="my-input" className="input-label"></label>
+                <input type="text" name="my-input"/>
+            </div>
+            <div className="input-container">
+                <label htmlFor="my-input" className="input-label"></label>
+                <input type="text" name="my-input"/>
+            </div>
+            <div className="input-container"></div>
+            <div className="input-container"></div>
+            <div className="input-container"></div>
             <label htmlFor="my-input">YO
                 <input name="my-input" type="text" />
             </label>
